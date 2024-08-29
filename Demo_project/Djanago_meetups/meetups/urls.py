ef745e605563d7c684c6ee187fa24e12bac1d91d
Ansar Ul Haq <ansarktk541@gmail.com>
2024-08-29 07:09:59 -0700

    Delete Demo_project/Djanago_meetups directory

--- Demo_project/Djanago_meetups/meetups/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns= [
-    path('meetups/',views.index) #our_domain.com/meetups/
- ]
--- Demo_project/Djanago_meetups/meetups/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    meetups = [
-        {'title': 'Django Meetup', 'location':'Newyork'},
-        {'title': 'Python Meetup', 'location':'Washnigton'},
-        {'title': 'Web Development Meetup', 'location':'Newyork'}
-    ]
-    return render(request, 'meetups/index.html', {'meetups': meetups})
